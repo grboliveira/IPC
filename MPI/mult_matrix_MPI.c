e0c79ef020195377741ea6072c59d202ca8c68a8
Guilherme Oliveira <grboliveira@sga.pucminas.br>
2018-07-20 16:37:55 -0300

    Adaptação de mult_matrix para MPI v1

--- MPI/mult_matrix_MPI.c
+++ MPI/mult_matrix_MPI.c
@@ -10,52 +10,71 @@
 /*
  *Multiplicação de matrizes com MPI
  */ 
-void mult_matrix(int *matA, int *matB,int argc, char **argv);
+void mult_matrix(int *matA, int *matB);
 
 
 
+int rank;
 int main(int argc, char **argv){
-	int *matA = malloc(NROWS * NCOLS * sizeof(int));
-	int *matB = malloc(NROWS * NCOLS * sizeof(int));
-	int numThread = 4;
-	srand(time(NULL));
-	for (int i = 0; i < NROWS; i++)
-	{
-		for (int j = 0; j < NCOLS; j++)
-		{
-			matA[(i*NROWS)+j] = rand() % 100;
-			matB[(i*NROWS)+j] = rand() % 100;
-		}
-	}
-	struct timeval start, end;
-  	gettimeofday(&start, NULL);
-	mult_matrix(matA,matB,argc,argv);
-	gettimeofday(&end, NULL);
-	printf("Tempo total:%lu\n",((end.tv_sec * 1000000 + end.tv_usec)
-		  - (start.tv_sec * 1000000 + start.tv_usec)));
-	return 0;
-}
-
-void mult_matrix(int *matA, int *matB,int argc, char **argv){
-	int *result = malloc(NROWS * NCOLS * sizeof(int));
-	int count = 0;
-	int numtasks;
-	int rank;	
 	MPI_Init(&argc, &argv);
+	int numtasks;
+	char flag [9];
 	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	printf("NumTasks:%d\nRank:%d\n",numtasks,rank);
 	if (rank == 0)
 	{
-		for (int i = rank; i < NROWS; i=i+numtasks)
+		int *matA = malloc(NROWS * NCOLS * sizeof(int));
+		int *matB = malloc(NROWS * NCOLS * sizeof(int));
+		srand(time(NULL));
+		for (int i = 0; i < NROWS; i++)
+		{
+			for (int j = 0; j < NCOLS; j++)
+			{
+				matA[(i*NROWS)+j] = rand() % 100;
+				matB[(i*NROWS)+j] = rand() % 100;
+			}
+		}
+		struct timeval start, end;
+	  	gettimeofday(&start, NULL);
+	  	MPI_Send(&matA,1,MPI_INT,1,0,MPI_COMM_WORLD);
+	  	MPI_Send(&matB,1,MPI_INT,1,1,MPI_COMM_WORLD);
+		mult_matrix(matA,matB);
+		MPI_Recv(&flag, 12, MPI_CHAR, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		gettimeofday(&end, NULL);
+		printf("Tempo total:%lu\n",((end.tv_sec * 1000000 + end.tv_usec)
+			  - (start.tv_sec * 1000000 + start.tv_usec)));
+			
+	}
+	if (rank == 1)
+	{
+		int *matA = malloc(NROWS * NCOLS * sizeof(int));
+		int *matB = malloc(NROWS * NCOLS * sizeof(int));
+		char message [] = "Finished";
+		MPI_Recv(&matA, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		MPI_Recv(&matB, 1, MPI_INT, 0, 1, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		mult_matrix(matA,matB);
+		MPI_Send(&message,9,MPI_CHAR,1,2,MPI_COMM_WORLD);
+	}
+	MPI_Finalize();
+	return 0;
+}
+
+
+void mult_matrix(int *matA, int *matB){
+	int *result = malloc(NROWS * NCOLS * sizeof(int));
+	int count = 0;
+
+	if (rank == 0)
+	{
+		for (int i = 0; i < (NROWS/2); i=i++)
 		{
 			for (int j = 0; j < NROWS; j++)
 			{
 				count = 0;
 				for (int k = 0; k < NCOLS ; k++)
 				{									
-					int msg = matA[(i*NROWS)+k]*matB[(k*NROWS)+j];
-					MPI_Send(&msg,1,MPI_INT,1,0,MPI_COMM_WORLD);	 
+					count += matA[(i*NROWS)+k]*matB[(k*NROWS)+j];	 
 				}
 				result[(i*NROWS)+j] = count;
 			}
@@ -65,11 +84,25 @@ void mult_matrix(int *matA, int *matB,int argc, char **argv){
 
 	if (rank == 1)
 	{
-		int sum;
-		MPI_Recv(&sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		count += sum;
-		printf("Count:%d\n", count);
+		for (int i = NROWS/2; i < NROWS; i=i++)
+		{
+			for (int j = 0; j < NROWS; j++)
+			{
+				count = 0;
+				for (int k = 0; k < NCOLS ; k++)
+				{									
+					count += matA[(i*NROWS)+k]*matB[(k*NROWS)+j];	 
+				}
+				result[(i*NROWS)+j] = count;
+			}
+			
+		}
 	}
+	
+	
+		
+	
+
 	
 	
 		//printf("Número de Threads:%d\n",omp_get_num_threads());
@@ -85,5 +118,5 @@ void mult_matrix(int *matA, int *matB,int argc, char **argv){
 		printf("|\n");
 	}
 	*/
-	MPI_Finalize();
+	
 }
