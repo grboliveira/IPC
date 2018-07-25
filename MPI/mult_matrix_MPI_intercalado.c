b7124a6959d920d30f850b57900063b0077ced10
Guilherme Oliveira <grboliveira@sga.pucminas.br>
2018-07-25 12:48:33 -0300

    Implementado o Mult_Matrix com MPI, adcionado Mult_Matrix com o maior número possível de comunicações

--- MPI/mult_matrix_MPI_intercalado.c
+++ MPI/mult_matrix_MPI_intercalado.c
@@ -18,7 +18,8 @@ int rank;
 int main(int argc, char **argv){
 	MPI_Init(&argc, &argv);
 	int numtasks;
-	char flag [9];
+	char flagA [9];
+	char flagB [9];
 	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	printf("NumTasks:%d\nRank:%d\n",numtasks,rank);
@@ -37,25 +38,48 @@ int main(int argc, char **argv){
 		}
 		struct timeval start, end;
 	  	gettimeofday(&start, NULL);
-	  	MPI_Send(matA,(NROWS*NCOLS),MPI_INT,1,0,MPI_COMM_WORLD);
-	  	MPI_Send(matB,(NROWS*NCOLS),MPI_INT,1,1,MPI_COMM_WORLD);
-		mult_matrix(matA,matB);
-		MPI_Recv(&flag, 9, MPI_CHAR, 1, 2, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  	MPI_Send(matB,(NROWS*NCOLS),MPI_INT,1,0,MPI_COMM_WORLD);
+	  	MPI_Send(matB,(NROWS*NCOLS),MPI_INT,2,0,MPI_COMM_WORLD);
+	  	int j = 1;
+	  	for (int i = 0; i < NROWS; i+=2, j++)
+	  	{
+	  		MPI_Send(matA[i],NCOLS,MPI_INT,1,j,MPI_COMM_WORLD);
+	  		MPI_Send(matA[i+1],NCOLS,MPI_INT,2,j,MPI_COMM_WORLD);
+	  		MPI_Recv(&flagA, 9, MPI_CHAR, 1,j, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  		MPI_Recv(&flagB, 9, MPI_CHAR, 2,j, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  	}
 		gettimeofday(&end, NULL);
-		printf("%s\n",flag);
+		printf("%s\n",flagA);
+		printf("%s\n",flagB);
 		printf("Tempo total:%lu\n",((end.tv_sec * 1000000 + end.tv_usec)
 			  - (start.tv_sec * 1000000 + start.tv_usec)));
 			
 	}
 	if (rank == 1)
 	{
-		int *matA = malloc(NROWS * NCOLS * sizeof(int));
+		int *matA = malloc(NCOLS * sizeof(int));
 		int *matB = malloc(NROWS * NCOLS * sizeof(int));
 		char message [] = "Finished";
-		MPI_Recv(matA, (NROWS*NCOLS), MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		MPI_Recv(matB, (NROWS*NCOLS), MPI_INT, 0, 1, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		mult_matrix(matA,matB);
-		MPI_Send(&message,9,MPI_CHAR,0,2,MPI_COMM_WORLD);
+		MPI_Recv(matB, (NROWS*NCOLS), MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		for (int i = 1; i < NCOLS;i++)
+		{
+			MPI_Recv(matA, NCOLS, MPI_INT, 0, i, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			mult_matrix(matA,matB);
+			MPI_Send(&message,9,MPI_CHAR,0,i,MPI_COMM_WORLD);	
+		}
+	}
+	if (rank == 2)
+	{
+		int *matA = malloc(NCOLS * sizeof(int));
+		int *matB = malloc(NROWS * NCOLS * sizeof(int));
+		char message [] = "Finished";
+		MPI_Recv(matB, (NROWS*NCOLS), MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		for (int i = 1; i < NCOLS; i++)
+		{
+			MPI_Recv(matA, NCOLS, MPI_INT, 0, 1, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			mult_matrix(matA,matB);
+			MPI_Send(&message,9,MPI_CHAR,0,2,MPI_COMM_WORLD);	
+		}
 	}
 	MPI_Finalize();
 	return 0;
@@ -122,3 +146,4 @@ void mult_matrix(int *matA, int *matB){
 	*/
 	
 }
+ 
