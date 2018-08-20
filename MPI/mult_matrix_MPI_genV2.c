6cf05797eeed47b98110c3e97bdeb5f7a4261b74
Guilherme Oliveira <grboliveira@sga.pucminas.br>
2018-08-20 16:39:11 -0300

    Solução finalizada do mult_matrix_genV2

--- MPI/mult_matrix_MPI_genV2.c
+++ MPI/mult_matrix_MPI_genV2.c
@@ -6,12 +6,13 @@
 #include <stdlib.h>
 #define NROWS  1000
 #define NCOLS  1000
+#define MEM_SIZE 1073741824
 
 /*
  *Multiplicação de matrizes com MPI
  */ 
-void mult_matrix(int *matA, int *matB, int size);
-
+int *mult_matrix(int *matA, int *matB, int size);
+int *transp_matrix(int *mat);
 
 
 int rank;
@@ -25,49 +26,68 @@ int main(int argc, char **argv){
 	{
 		int *matA = malloc(NROWS * NCOLS * sizeof(int));
 		int *matB = malloc(NROWS * NCOLS * sizeof(int));
+		int *matC = malloc(NROWS * NCOLS * sizeof(int));
 		srand(time(NULL));
 		for (int i = 0; i < NROWS; i++)
 		{
 			for (int j = 0; j < NCOLS; j++)
 			{
-				matA[(i*NROWS)+j] = rand() % 100;
-				matB[(i*NROWS)+j] = rand() % 100;
+				matA[(i*NROWS)+j] = rand() % 1000;
+				matB[(i*NROWS)+j] = rand() % 1000;
 			}
 		} 
 		struct timeval start, end;
 	  	gettimeofday(&start, NULL);	
 	  	int sizepart = NROWS/(numtasks-1);
+	  	int subsizepart = ((sizepart*NCOLS)*sizeof(int))/MEM_SIZE;
 	  	for (int i = 1; i < numtasks; i++)
 	  	{
-	  		MPI_Send(&sizepart,1,MPI_INT,i,0,MPI_COMM_WORLD);
+	  		MPI_Send(&subsizepart,1,MPI_INT,i,0,MPI_COMM_WORLD);
 	  	}
 	  	//printf("Saiu do for que envia o tamanho\n");
-	  	int *buffer = malloc(sizepart * NROWS * sizeof(int));
 	  	int abort = 0;
-	  	for (int i = 0; i < NCOLS ; i=i+sizepart)
+	  	int *matransp = transp_matrix(matB);
+	  	for (int i = 0; i < subsizepart; i=i+subsizepart)
 	  	{
 	  		for (int j = 1 ; j < numtasks;j++)
-	 		{
-	 			//printf("Master:Entrou no for\n");
-	 			//printf("sizepart*N:%d\n",sizepart*NCOLS );
+			{
+	 			//printf("Master:NROWS/sizepart:%d\n",NROWS/sizepart);
+	 			//printf("sizepart*N:%d\n",sizepart*NCOLS);
 	 			MPI_Send(&abort,1,MPI_INT,j,3,MPI_COMM_WORLD);
-	  			MPI_Send(&matA[i*sizepart],sizepart*NCOLS,MPI_INT,j,1,MPI_COMM_WORLD);
+	  			MPI_Send(&matA[(j-1)*subsizepart],subsizepart,MPI_INT,j,1,MPI_COMM_WORLD);
 	  			//printf("Enviou o matA\n"); 		
-	  			//printf("(J-1)%d\n", j-1);
-	  			int pos = (j-1) * i;
-	  			for (int k = 0; k < sizepart*NROWS; k++)
-				{	
-					//printf("K*NCOLS = %d\n",k*NCOLS);
-					buffer[k] = matB[pos*NCOLS];
-				}		  			
-				//printf("Preencheu o buffer\n"); 		
-	  			MPI_Send(&buffer[0],sizepart*NROWS,MPI_INT,j,2,MPI_COMM_WORLD);
-	  			//printf("Enviou o buffer\n"); 		
-	  		}	
-	  	}
+	  			//printf("(J-1)%d\n", j-1); 		
+	  			MPI_Send(&matransp[i*subsizepart],subsizepart,MPI_INT,j,2,MPI_COMM_WORLD);
+	  			//printf("Enviou o buffer\n");
+	 					
+	  		}
+	  		for (int j = 1; j < numtasks; j++)
+	  		{
+	  			//printf("Entrou no for do result\n");
+	  			int *resultpart = malloc(subsizepart * subsizepart * sizeof(int));
+	  			//printf("Master:Alocou o resultpart\n");
+	  			MPI_Recv(resultpart, subsizepart, MPI_INT, j, 4, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  			//printf("Master:Recebeu o resultado\n");
+	  			int pos = i*NCOLS;
+	  			for(int k = 0 ;k < (subsizepart/NCOLS); k++)
+	  			{
+	  				for(int l = 0;l < (subsizepart/NROWS);l++)
+	  				{
+	  					matC[((i+k)*NCOLS)+l] = resultpart[((i+k)*(subsizepart/NROWS))+l];
+	  				}
+	  			}
+	  			//printf("Master:Recebeu o resultado:j%d\n",j);
+	  		}
+	  		//printf("Master:Executou a leva%d\n",i );
+
+	  	}	
+	  		
+	  		
+	  			
+	  	
 	  		
 	  	
-	  	
+	  	//printf("Saiu da mult\n");
 	  
 		gettimeofday(&end, NULL);
 		abort = 1; 
@@ -97,31 +117,30 @@ int main(int argc, char **argv){
 			//printf("Slave:Recebeu as fileiras\n");
 			MPI_Recv(columns, size*NROWS, MPI_INT, 0, 2, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 			//printf("Slave:Recebeu as linhas e colunas\n");
-			mult_matrix(rows,columns,size);	
+			int *result = mult_matrix(rows,columns,size);
+			//printf("Slave%d:Calculou a mult_matrix\n",rank);	
+			MPI_Send(&result[0],size*size,MPI_INT,0,4,MPI_COMM_WORLD);	
+			//printf("Slave%d:Enviou o result\n",rank);
 		}while(1);		
 	}
 	MPI_Finalize();
 	return 0;
 }
 
-void mult_matrix(int *matA, int *matB,int size){
-	int *result = malloc(NROWS * NCOLS * sizeof(int));
+int *mult_matrix(int *matA, int *matB,int size){
+	int *result = malloc(size * sizeof(int));
 	int count = 0;
 
 
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < (size/NCOLS); i++)
 	{
 
-		for (int j = 0; j < size; j++)
+		for (int j = 0; j < (size/NROWS); j++)
 		{
 			count = 0;
-			for (int k = 0; k < size ; k++)
-			{									
-				count += matA[(i*NROWS)+k]*matB[(k*NROWS)+j];	
-			}
-			result[(i*NROWS)+j] = count;
+			count += matA[(i*NROWS)+j]*matB[(i*NROWS)+j];		
 		}
-			
+		result[i] = count;
 	}
 	//printf("Slave:Executou a mult_matrix\n");
 		
@@ -142,5 +161,19 @@ void mult_matrix(int *matA, int *matB,int size){
 		printf("|\n");
 	}
 	*/
+
+	return result;
 	
+}
+
+int *transp_matrix(int *mat){
+	int *transp = malloc(NROWS * NCOLS * sizeof(int));
+	for (int i = 0; i < NROWS ; i++)
+	{
+		for (int j = 0; i < NCOLS; ++i)
+		{
+			transp[(i*NCOLS)+j] = mat[(j*NCOLS)+i];	
+		}
+	}
+	return transp;
 }
