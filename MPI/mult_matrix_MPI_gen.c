6f50a1b48bb5da662158edc2834f4f523ace9a1c
Guilherme Oliveira <grboliveira@sga.pucminas.br>
2018-10-22 10:11:41 -0300

    Ready for Cart

--- MPI/mult_matrix_MPI_gen.c
+++ MPI/mult_matrix_MPI_gen.c
@@ -10,75 +10,6 @@
 /*
  *Multiplicação de matrizes com MPI
  */ 
-void mult_matrix(int *matA, int *matB);
-
-
-
-int rank;
-int main(int argc, char **argv){
-	MPI_Init(&argc, &argv);
-	int numtasks;
-	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	printf("NumTasks:%d\nRank:%d\n",numtasks,rank);
-	if (rank == 0)
-	{
-		int *matA = malloc(NROWS * NCOLS * sizeof(int));
-		int *matB = malloc(NROWS * NCOLS * sizeof(int));
-		srand(time(NULL));
-		for (int i = 0; i < NROWS; i++)
-		{
-			for (int j = 0; j < NCOLS; j++)
-			{
-				matA[(i*NROWS)+j] = rand() % 100;
-				matB[(i*NROWS)+j] = rand() % 100;
-			}
-		}
-		struct timeval start, end;
-	  	gettimeofday(&start, NULL);
-	  	for (int i = 1; i < numtasks; i++)
-	  	{
-	  		MPI_Send(matB,(NROWS*NCOLS),MPI_INT,i,0,MPI_COMM_WORLD);
-	  	}	
-	  	int i = 0;
-	  	int rank_recv;
-	  	do{
-	  		MPI_Recv(&rank_recv, 1, MPI_INT, MPI_ANY_SOURCE,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-	  		MPI_Send(&i,1,MPI_INT,rank_recv,2,MPI_COMM_WORLD);
-	  		MPI_Send(&matA[i],NCOLS,MPI_INT,rank_recv,1,MPI_COMM_WORLD);
-	 		i++;
-	  	}while(i < NROWS);
-		gettimeofday(&end, NULL);
-		int j = 1;
-		do{
-			MPI_Recv(&rank_recv, 1, MPI_INT, MPI_ANY_SOURCE,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-	  		MPI_Send(&i,1,MPI_INT,rank_recv,2,MPI_COMM_WORLD);
-	  		j++;
-		}while(j < numtasks);
-		printf("Finished\n");
-		printf("Tempo total:%lu\n",((end.tv_sec * 1000000 + end.tv_usec)
-			  - (start.tv_sec * 1000000 + start.tv_usec)));
-			
-	}
-	else if (rank != 0)
-	{
-		int i = 0;
-		int *matA = malloc(NCOLS * sizeof(int));
-		int *matB = malloc(NROWS * NCOLS * sizeof(int));
-		MPI_Recv(matB, (NROWS*NCOLS), MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		do{
-			MPI_Send(&rank,1,MPI_INT,0,1,MPI_COMM_WORLD);
-			MPI_Recv(&i, 1, MPI_INT, 0, 2, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			if (i < NCOLS)
-			{
-				MPI_Recv(matA, NCOLS, MPI_INT, 0, 1, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-				mult_matrix(matA,matB);	
-			}
-		}while(i < NCOLS);		
-	}
-	MPI_Finalize();
-	return 0;
-}
 
 void mult_matrix(int *matA, int *matB){
 	int *result = malloc(NROWS * NCOLS * sizeof(int));
@@ -113,3 +44,76 @@ void mult_matrix(int *matA, int *matB){
 	*/
 	
 }
+
+void populate_matrix(int *mat){
+	srand(time(NULL));
+	for(int i = 0; i < NROWS; i++){
+		for(int j = 0; j < NCOLS;j++){
+			mat[(i*NROWS)+j] = rand() % 100;
+		}
+	}
+}	
+
+
+int rank;
+int main(int argc, char **argv){
+	MPI_Init(&argc, &argv);
+	int numtasks;
+	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	printf("NumTasks:%d\nRank:%d\n",numtasks,rank);
+	if (rank == 0)
+	{
+		int *matA = malloc(NROWS * NCOLS * sizeof(int));
+		int *matB = malloc(NROWS * NCOLS * sizeof(int));
+		struct timeval start, end;
+		int i;
+		int j;
+		populate_matrix(matA);
+		populate_matrix(matB);
+	  	gettimeofday(&start, NULL);
+	  	for (int i = 1; i < numtasks; i++)
+	  	{
+	  		MPI_Send(matB,(NROWS*NCOLS),MPI_INT,i,0,MPI_COMM_WORLD);
+	  	}	
+	  	i = 0;
+	  	int rank_recv;
+	  	do{
+	  		MPI_Recv(&rank_recv, 1, MPI_INT, MPI_ANY_SOURCE,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  		MPI_Send(&i,1,MPI_INT,rank_recv,2,MPI_COMM_WORLD);
+	  		MPI_Send(&matA[i],NCOLS,MPI_INT,rank_recv,1,MPI_COMM_WORLD);
+	 		i++;
+	  	}while(i < NROWS);
+		gettimeofday(&end, NULL);
+		j = 1;
+		do{
+			MPI_Recv(&rank_recv, 1, MPI_INT, MPI_ANY_SOURCE,MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  		MPI_Send(&i,1,MPI_INT,rank_recv,2,MPI_COMM_WORLD);
+	  		j++;
+		}while(j < numtasks);
+		printf("Finished\n");
+		printf("Tempo total:%lu\n",((end.tv_sec * 1000000 + end.tv_usec)
+			  - (start.tv_sec * 1000000 + start.tv_usec)));
+			
+	}
+	else if (rank != 0)
+	{
+		int i = 0;
+		int *matA = malloc(NCOLS * sizeof(int));
+		int *matB = malloc(NROWS * NCOLS * sizeof(int));
+		MPI_Recv(matB, (NROWS*NCOLS), MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		do{
+			MPI_Send(&rank,1,MPI_INT,0,1,MPI_COMM_WORLD);
+			MPI_Recv(&i, 1, MPI_INT, 0, 2, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			if (i < NCOLS)
+			{
+				MPI_Recv(matA, NCOLS, MPI_INT, 0, 1, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+				mult_matrix(matA,matB);	
+			}
+		}while(i < NCOLS);		
+	}
+	MPI_Finalize();
+	return 0;
+}
+
+
--- MPI/mult_matrix_MPI_genV2.c
+++ MPI/mult_matrix_MPI_genV2.c
@@ -4,145 +4,28 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NROWS  1000
-#define NCOLS  1000
-#define MEM_SIZE 1073741824
+#define NROWS  4096
+#define NCOLS  4096
+#define MEM_SIZE 64
 
 /*
  *Multiplicação de matrizes com MPI
  */ 
-int *mult_matrix(int *matA, int *matB, int size);
-int *transp_matrix(int *mat);
-
-
-int rank;
-int main(int argc, char **argv){
-	MPI_Init(&argc, &argv);
-	int numtasks;
-	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	printf("NumTasks:%d\nRank:%d\n",numtasks,rank);
-	if (rank == 0)
-	{
-		int *matA = malloc(NROWS * NCOLS * sizeof(int));
-		int *matB = malloc(NROWS * NCOLS * sizeof(int));
-		int *matC = malloc(NROWS * NCOLS * sizeof(int));
-		srand(time(NULL));
-		for (int i = 0; i < NROWS; i++)
-		{
-			for (int j = 0; j < NCOLS; j++)
-			{
-				matA[(i*NROWS)+j] = rand() % 1000;
-				matB[(i*NROWS)+j] = rand() % 1000;
-			}
-		} 
-		struct timeval start, end;	
-	  	int sizepart = NROWS/(numtasks-1);
-	  	int subsizepart = ((sizepart*NCOLS)*sizeof(int))/MEM_SIZE;
-	  	for (int i = 1; i < numtasks; i++)
-	  	{
-	  		MPI_Send(&subsizepart,1,MPI_INT,i,0,MPI_COMM_WORLD);
-	  	}
-	  	//printf("Saiu do for que envia o tamanho\n");
-	  	int abort = 0;
-	  	int *matransp = transp_matrix(matB);
-		gettimeofday(&start, NULL);	
-	  	for (int i = 0; i < subsizepart; i=i+subsizepart)
-	  	{
-	  		for (int j = 1 ; j < numtasks;j++)
-			{
-	 			//printf("Master:NROWS/sizepart:%d\n",NROWS/sizepart);
-	 			//printf("sizepart*N:%d\n",sizepart*NCOLS);
-	 			MPI_Send(&abort,1,MPI_INT,j,3,MPI_COMM_WORLD);
-	  			MPI_Send(&matA[(j-1)*subsizepart],subsizepart,MPI_INT,j,1,MPI_COMM_WORLD);
-	  			//printf("Enviou o matA\n"); 		
-	  			//printf("(J-1)%d\n", j-1); 		
-	  			MPI_Send(&matransp[i*subsizepart],subsizepart,MPI_INT,j,2,MPI_COMM_WORLD);
-	  			//printf("Enviou o buffer\n");
-	 					
-	  		}
-	  		for (int j = 1; j < numtasks; j++)
-	  		{
-	  			//printf("Entrou no for do result\n");
-	  			int *resultpart = malloc(subsizepart * subsizepart * sizeof(int));
-	  			//printf("Master:Alocou o resultpart\n");
-	  			MPI_Recv(resultpart, subsizepart, MPI_INT, j, 4, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-	  			//printf("Master:Recebeu o resultado\n");
-	  			int pos = i*NCOLS;
-	  			for(int k = 0 ;k < (subsizepart/NCOLS); k++)
-	  			{
-	  				for(int l = 0;l < (subsizepart/NROWS);l++)
-	  				{
-	  					matC[((i+k)*NCOLS)+l] = resultpart[((i+k)*(subsizepart/NROWS))+l];
-	  				}
-	  			}
-	  			//printf("Master:Recebeu o resultado:j%d\n",j);
-	  		}
-	  		//printf("Master:Executou a leva%d\n",i );
-
-	  	}	
-	  		
-	  		
-	  			
-	  	
-	  		
-	  	
-	  	//printf("Saiu da mult\n");
-	  
-		gettimeofday(&end, NULL);
-		abort = 1; 
-		for (int i = 1; i < numtasks; i++)
-		{
-			MPI_Send(&abort,1,MPI_INT,i,3,MPI_COMM_WORLD);
-		}
-			
-		printf("Finished\n");
-		printf("Tempo total:%lu\n",((end.tv_sec * 1000000 + end.tv_usec)
-			  - (start.tv_sec * 1000000 + start.tv_usec)));
-			
-	}	
-	else if (rank != 0)
-	{
-		int size;
-		int abort;
-		MPI_Recv(&size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-		int *rows = malloc(NCOLS * size * sizeof(int));
-		int *columns = malloc(NROWS * size * sizeof(int));
-		do{
-			MPI_Recv(&abort, 1, MPI_INT, 0, 3, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			if (abort == 1)
-				break;
-			//printf("Slave:Entrou no dowhile\n");
-			MPI_Recv(rows, size*NCOLS, MPI_INT, 0, 1, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			//printf("Slave:Recebeu as fileiras\n");
-			MPI_Recv(columns, size*NROWS, MPI_INT, 0, 2, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			//printf("Slave:Recebeu as linhas e colunas\n");
-			int *result = mult_matrix(rows,columns,size);
-			//printf("Slave%d:Calculou a mult_matrix\n",rank);	
-			MPI_Send(&result[0],size*size,MPI_INT,0,4,MPI_COMM_WORLD);	
-			//printf("Slave%d:Enviou o result\n",rank);
-		}while(1);		
-	}
-	MPI_Finalize();
-	return 0;
-}
 
 int *mult_matrix(int *matA, int *matB,int size){
-	int *result = malloc(size * sizeof(int));
+	int *result = malloc(size * size * sizeof(int));
 	int count = 0;
 
 
-	for (int i = 0; i < (size/NCOLS); i++)
+	for (int i = 0; i < size; i++)
 	{
-
-		for (int j = 0; j < (size/NROWS); j++)
+		for (int j = 0; j < size; j++)
 		{
 			count = 0;
-			count += matA[(i*NROWS)+j]*matB[(i*NROWS)+j];		
+			count += matA[(i*size)+j]*matB[(i*size)+j];		
 		}
 		result[i] = count;
 	}
-	//printf("Slave:Executou a mult_matrix\n");
 		
 	
 	
@@ -177,3 +60,111 @@ int *transp_matrix(int *mat){
 	}
 	return transp;
 }
+
+void populate_matrix(int *mat){
+	srand(time(NULL));
+	for(int i = 0; i < NROWS; i++){
+		for(int j = 0; j < NCOLS; j++){
+			mat[(i*NROWS)+j] = rand() % 100;
+		}
+	}
+}	
+
+
+int rank;
+int main(int argc, char **argv){
+	MPI_Init(&argc, &argv);
+	int numtasks;
+	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	if (rank == 0)
+	{
+		int *matA = malloc(NROWS * NCOLS * sizeof(int));
+		int *matB = malloc(NROWS * NCOLS * sizeof(int));
+		int *matC = malloc(NROWS * NCOLS * sizeof(int));	
+		struct timeval start, end;	
+	  	int sizepart = NROWS/(numtasks-1);
+	  	int subsizepart = sizepart/MEM_SIZE;
+		int piecemat = subsizepart*subsizepart;
+		int num_pieces = NROWS/subsizepart;
+		int abort;
+		int pos;
+		int *matransp;
+		populate_matrix(matA);
+		populate_matrix(matB);
+	  	for (int i = 1; i < numtasks; i++)
+	  	{
+	  		MPI_Send(&subsizepart,1,MPI_INT,i,0,MPI_COMM_WORLD);
+	  	}
+	  	abort = 0;
+	  	matransp = transp_matrix(matB);
+		gettimeofday(&start, NULL);	
+	  	for (int iS = 0; iS < num_pieces  ; iS++)
+	  	{
+	  		for(int i = 0;i < num_pieces; i+=(numtasks-1))
+			{
+				for (int j = 1 ; j < numtasks;j++)
+				{
+	 				MPI_Send(&abort,1,MPI_INT,j,3,MPI_COMM_WORLD);
+	  				MPI_Send(&matA[(i+(j-1))*piecemat],piecemat,MPI_INT,j,1,MPI_COMM_WORLD);
+	  				MPI_Send(&matransp[iS*piecemat],piecemat,MPI_INT,j,2,MPI_COMM_WORLD);
+	 					
+	  			}
+	  			for (int j = 1; j < numtasks; j++)
+	  			{
+					int *resultpart = malloc(piecemat * sizeof(int));
+					int m = 0;
+					int count = 0;
+	  				MPI_Recv(resultpart, piecemat, MPI_INT, j, 4, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  				pos = i*NCOLS;
+	  				for(int k = 0 ;m < piecemat; k+=(NCOLS-subsizepart))
+	  				{
+				
+						count++;
+	  					for(int l = 0;l < subsizepart;l++,m++)
+	  					{
+							matC[((i+(j-1))*piecemat)+k+l] = resultpart[m];
+						}
+	  				}
+	  			}
+
+	  		}
+		}	
+	  		
+	  		
+	  			
+	  
+		gettimeofday(&end, NULL);
+		abort = 1; 
+		for (int i = 1; i < numtasks; i++)
+		{
+			MPI_Send(&abort,1,MPI_INT,i,3,MPI_COMM_WORLD);
+		}
+			
+		printf("Finished\n");
+		printf("Tempo total:%lu\n",((end.tv_sec * 1000000 + end.tv_usec)
+			  - (start.tv_sec * 1000000 + start.tv_usec)));
+			
+	}	
+	else if (rank != 0)
+	{
+		int size;
+		int abort;
+		MPI_Recv(&size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		int *rows = malloc(size* size * sizeof(int));
+		int *columns = malloc(size * size * sizeof(int));
+		do{
+			MPI_Recv(&abort, 1, MPI_INT, 0, 3, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			if (abort == 1)
+				break;
+			MPI_Recv(rows, size*size, MPI_INT, 0, 1, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			MPI_Recv(columns, size*size, MPI_INT, 0, 2, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+			int *result = mult_matrix(rows,columns,size);
+			MPI_Send(&result[0],size*size,MPI_INT,0,4,MPI_COMM_WORLD);	
+		}while(1);		
+	}
+	MPI_Finalize();
+	return 0;
+}
+
+
